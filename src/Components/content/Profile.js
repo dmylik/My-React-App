46c4531a7ecd380060f0c49625fb8951ddc6577b
Дмитрий Мялик <mylik.dima@gmail.com>
2021-10-29 17:01:56 +0300

    - Profile Container

--- src/Components/content/Profile.js
+++ src/Components/content/Profile.js
@@ -2,50 +2,33 @@ import React from 'react'
 import pm from '../../Style/content/Profile.module.css'
 import Post from "./Post/Post";
 import fordMustang from '../../assets/photo/ford.mustamg69.jpg'
-import * as axios from "axios";
 
 // Класовая компанента Profile
-class ProfileClass extends React.Component {
-
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-            .then(response => {
-                // this.props.setUserProfile(response.data);
-
-
-            });
-    }
-
-    ollPostElement = this.props.ollPost.postArray.map(p => <Post message={p.post} like={p.likes} key={p.id}/>);
-    newPostElement = this.props.newPostText;
-
-    onAddPost = () => {this.props.addPost();};
-    onPostChange = (e) => {this.props.upTxtP(e.target.value);};
-
-    render() {
-        return(
-            <div className={pm.item}>
-                <div className={pm.logoImage}>
-                    <img src={fordMustang}/>
-                </div>
-                <div>
-                    ProfileInfo
-                </div>
-                <div className={pm.posts}>
-                    <h3>My Post</h3>
-                    <div className={pm.addPost}>
-                        <div>
-                            <textarea value={this.newPostElement} onChange={this.onPostChange}/>
-                        </div>
-                        <div>
-                            <button onClick={this.onAddPost}>Add Post</button>
-                        </div>
+const ProfileClass = (props) => {
+    return(
+        <div className={pm.item}>
+            <div className={pm.logoImage}>
+                <img src={fordMustang}/>
+            </div>
+            <div>
+                ProfileInfo
+            </div>
+            <div className={pm.posts}>
+                <h3>My Post</h3>
+                <div className={pm.addPost}>
+                    <div>
+                        <textarea value={props.newPostElement} onChange={props.onPostChange}/>
                     </div>
-                    {this.ollPostElement}
+                    <div>
+                        <button onClick={props.onAddPost}>Add Post</button>
+                    </div>
                 </div>
-            </div>)
-    }
-}
+                {props.ollPostElement}
+            </div>
+        </div>
+    )
+};
+
 
 // Презентационная (функциональная) компонента Profile
 const ProfileFunction = (props) => {
@@ -85,7 +68,8 @@ const ProfileFunction = (props) => {
             </div>
         </div>
     )
-}
+};
 
 
-export default ProfileFunction
+export default ProfileClass
+// export default ProfileFunction
--- src/Components/content/ProfileContainer.js
+++ src/Components/content/ProfileContainer.js
@@ -1,44 +1,38 @@
 import React from 'react'
 import {addPostActionCreator, updateNewTextPostActionCreator} from "../../redux/reducers/profile-reducer";
+import ProfileAPI from "./Profile";
 import Profile from "./Profile";
 import {connect} from "react-redux";
+import * as axios from "axios";
+import Post from "./Post/Post";
 
-// Контенерная компонента который получает данные и передает их компонете Profile
-// const ProfileContainer = (props) => {
-//     let state = props.store.getState();
-//     let addPost = ()=> {props.store.dispatch(addPostActionCreator());};
-//     let onPostChange = (text) => {
-//         props.store.dispatch (updateNewTextPostActionCreator(text));};
-//     return
-//         <Profile upTxtP={onPostChange}   //callback
-//                  addPost={addPost}                  //callback
-//                  ollPost={state.getState().profilePage}   //данные
-//                  newPostText={state.profilePage.newPostText}/>
-// };
-// const ProfileContainer = (props) => {
-//     //let state = props.store.getState();
-//     // let addPost = ()=> {props.store.dispatch(addPostActionCreator());};
-//     // let onPostChange = (text) => {
-//     //     props.store.dispatch (updateNewTextPostActionCreator(text));};
-//     return (
-//         <StoreContext.Consumer>
-//             {
-//             (store)=> {
-//                 let state = store.getState();
-//
-//                 let addPost = ()=> {store.dispatch(addPostActionCreator());};
-//                 let onPostChange = (text) => {
-//                    store.dispatch (updateNewTextPostActionCreator(text));};
-//                 return <Profile upTxtP={onPostChange}   //callback
-//                      addPost={addPost}                  //callback
-//                      ollPost={state.profilePage}   //данные
-//                      // ollPost={state.getState().profilePage}   //данные
-//                      newPostText={state.profilePage.newPostText}/>
-//             }
-//             }
-//         </StoreContext.Consumer>
-//     )
-// };
+
+
+class ProfileClass extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .then(response => {
+                // this.props.setUserProfile(response.data);
+
+
+            });
+    }
+    ollPostElement = this.props.ollPost.postArray.map(p => <Post message={p.post} like={p.likes} key={p.id}/>);
+    newPostElement = this.props.newPostText;
+
+    onAddPost = () => {this.props.addPost();};
+    onPostChange = (e) => {this.props.upTxtP(e.target.value);};
+
+    render() {
+        return(<ProfileAPI newPostElement={this.newPostElement}
+                           ollPostElement={this.ollPostElement}
+                           onAddPost={this.onAddPost}
+                           onPostChange={this.onPostChange}
+
+        />)
+    }
+};
+
 
 // Функция которая передает данные
 let mapStateToProps = (state)=> {
@@ -57,7 +51,7 @@ let mapDispatchToProps = (dispatch) => {
 };
 
 //Контенерная компонента который получает данные и передает их компонете Profile
-const ProfileContainer = connect(mapStateToProps,mapDispatchToProps)(Profile);
+const ProfileContainer = connect(mapStateToProps,mapDispatchToProps)(ProfileClass);
 
 
 export default ProfileContainer;
